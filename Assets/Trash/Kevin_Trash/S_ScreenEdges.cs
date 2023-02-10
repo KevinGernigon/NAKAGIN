dfbdc3ac2632223d007de57c01b5f5f5c9fbda44
KevinGernigon <k.gernigon@rns.ecolescreatives.com>
2023-02-10 08:51:28 +0100

    scene_manager

---
+++ Assets/Trash/Kevin_Trash/S_ScreenEdges.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class S_ScreenEdges : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _player;
+    [SerializeField]
+    private List<Collider> _colliderList;
+
+    [SerializeField]
+    private Image _screenEdges;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
