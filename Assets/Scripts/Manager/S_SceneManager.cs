e595011ebf3e7cf8c09455c1cf0bf03f6b9bad6d
Tom Sachot <t.sachot@rns.ecolescreatives.com>
2023-03-03 12:00:19 +0100

    LD Hub 2 Level Validation Mid

--- Assets/Scripts/Manager/S_SceneManager.cs
+++ Assets/Scripts/Manager/S_SceneManager.cs
@@ -10,7 +10,7 @@ public class S_SceneManager : Manager
         Manager_Scene,
         Assets_Scene,
         Corentin_Scene,
-        Kiki_Scene
+        Kiki_Scene,Tom_Scene
 
     }
 
@@ -32,7 +32,7 @@ public class S_SceneManager : Manager
 
     public void LoadGame()
     {
-        SceneManager.LoadScene(Scene.Kiki_Scene.ToString());
+        SceneManager.LoadScene(Scene.Tom_Scene.ToString());
     }
 
 
