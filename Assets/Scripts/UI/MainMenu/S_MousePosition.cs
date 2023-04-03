c34cb9a9976e124bcf84bd6d5d3e4841f99b2b1b
Corentin-Guerin <cocog53200@gmail.com>
2023-04-03 11:33:06 +0200

    gestion de mainmenu

--- Assets/Scripts/UI/MainMenu/S_MousePosition.cs
+++ Assets/Scripts/UI/MainMenu/S_MousePosition.cs
@@ -1,22 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class S_MousePosition : MonoBehaviour
 {
     [SerializeField] private Camera _mainCamera;
     [SerializeField] private LayerMask LayerMask;
     [SerializeField] private Transform _nakagin;
+    [SerializeField] private GameObject _target;
+    [SerializeField] private GameObject _NewGameGameObject;
 
+
+    private S_ReferenceInterface _referenceInterface;
+    private S_InputManager _InputManager;
+    private GameObject _selectedGameObject;
+
+    private void Awake()
+    {
+        _referenceInterface = S_GestionnaireManager.GetManager<S_ReferenceInterface>();
+        _InputManager = _referenceInterface._InputManager;
+
+
+    }
+    private void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(_NewGameGameObject);
+        
+    }
     private void Update()
     {
-     
-      
-       Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
-
-        if (Physics.Raycast(ray, out RaycastHit raycasthit, float.MaxValue,LayerMask))
+        if (_InputManager._playerInput.currentControlScheme == "Gamepad")
         {
-            transform.position = raycasthit.point;
+            _target.transform.position = Vector3.zero;
+
+           if (EventSystem.current.currentSelectedGameObject != null)
+           {
+                _selectedGameObject = EventSystem.current.currentSelectedGameObject;
+                _target.transform.SetParent(_selectedGameObject.transform);
+
+                Ray ray = _mainCamera.ScreenPointToRay(_selectedGameObject.transform.position);
+                if (Physics.Raycast(ray, out RaycastHit raycasthit, float.MaxValue, LayerMask))
+                {
+                    _target.transform.position = raycasthit.point;
+                }
+           }
+        }
+
+        if (_InputManager._playerInput.currentControlScheme == "KeyboardAndMouse")
+        {
+            _target.transform.SetParent(gameObject.transform);
+            Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit raycasthit, float.MaxValue,LayerMask))
+            {
+                _target.transform.position = raycasthit.point;
+            }
         }
 
         _nakagin.LookAt(transform.position);
---
+++ Assets/Trash/Corentin_Trash/S_GestionMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_GestionMenu : MonoBehaviour
+{
+    private S_ReferenceInterface _referenceInterface;
+    private S_InputManager _InputManager;
+    private GameObject _disableManager;
+
+        
+    private void Awake()
+    {
+        _referenceInterface = S_GestionnaireManager.GetManager<S_ReferenceInterface>();
+        _InputManager = _referenceInterface._InputManager;
+        _disableManager = _referenceInterface.DisableManager;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+
+    private void Start()
+    {
+        _disableManager.SetActive(false);
+        _InputManager.ActivePause();
+
+    }
+
+    public void EndScene()
+    {
+        _disableManager.SetActive(true);
+        _InputManager.DesactivePause();
+    }
+}
