88f8d20dc54077a8b982e8330538a840346e24c3
FrabouletAlexis <74670007+FrabouletAlexis@users.noreply.github.com>
2023-06-15 17:04:03 +0200

    avancement trial

--- Assets/Scripts/LevelDesign/CheckPoints/S_CheckPointValidation.cs
+++ Assets/Scripts/LevelDesign/CheckPoints/S_CheckPointValidation.cs
@@ -7,19 +7,22 @@ public class S_CheckPointValidation : MonoBehaviour
     [SerializeField] private Transform _capsuleCheckpointArea;
     //[SerializeField] private Transform _capsuleRespawn;
     [SerializeField] private S_RunCheckPointManagerValidation _RunCheckPointManagerValidation;
+    [SerializeField] private Animator _Animcheckpoint;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             //Debug.Log("checkPoint");
-            _RunCheckPointManagerValidation.checkpointCapsule.transform.position = _capsuleCheckpointArea.transform.position;
-            _RunCheckPointManagerValidation.checkpointCapsule.transform.position = new Vector3(_RunCheckPointManagerValidation.checkpointCapsule.transform.position.x, _RunCheckPointManagerValidation.checkpointCapsule.transform.position.y, _RunCheckPointManagerValidation.checkpointCapsule.transform.position.z);
+            
+            _RunCheckPointManagerValidation.checkpointCapsule.transform.position = new Vector3(_capsuleCheckpointArea.transform.position.x, _capsuleCheckpointArea.transform.position.y, _capsuleCheckpointArea.transform.position.z);
 
 
             _RunCheckPointManagerValidation.checkpointCapsule.transform.eulerAngles = new Vector3(_capsuleCheckpointArea.transform.rotation.eulerAngles.x, _capsuleCheckpointArea.transform.rotation.eulerAngles.y, _capsuleCheckpointArea.transform.rotation.eulerAngles.z);
 
             Physics.SyncTransforms();
+
+            _Animcheckpoint.SetBool("IsOpen", true);
         }
     }
 
